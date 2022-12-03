02624e4ad833291f255486af5ed4ffb809e6791d
Lu-PY <lpy102817@gmail.com>
2022-12-03 15:06:52 +0800

    add hello.js & sum.js

---
+++ sum.js
@@ -0,0 +1,14 @@
+function sum(n){
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+console.log(sum(1))
+console.log(sum(2))
+console.log(sum(3))
+console.log(sum(5))
+console.log(sum(7))
+console.log(sum(9))
+console.log(sum(10))
